34c0354aa15232142e971f19e04f5212f8b1f4ef
tanxiaohui <xiaohui.tan@greatdb.com>
2023-05-12 15:24:14 +0800

    遍历文件夹

---
+++ 遍历文件夹/traverse_folder.py
@@ -0,0 +1,42 @@
+import os
+
+
+def traverse_folder(path: str, n: int = 99):
+    path = os.path.abspath(path)
+    assert os.path.isdir(path)
+    assert n > 0
+    all_files = []
+    all_dirs = []
+    for root, dirs, files in os.walk(path):
+        for one_file in files:
+            all_files.append(os.path.join(root, one_file))  # 所有文件
+        for one_dir in dirs:
+            all_dirs.append(os.path.join(root, one_dir))  # 所有文件夹
+
+    path_split = path.split(os.sep)
+    len_path_split = len(path_split)
+
+    need_dir = []
+    for d_name in all_dirs:
+        dir_split = d_name.split(os.sep)
+        dir_split_ = dir_split[len_path_split:]
+        if len(dir_split_) <= n:
+            need_dir.append(d_name)
+
+    need_files = []
+    for f_name in all_files:
+        f_name_split = f_name.split(os.sep)
+        f_name_split_ = f_name_split[len_path_split:]
+        if len(f_name_split_) <= n:
+            need_files.append(f_name)
+
+    return need_dir, need_files
+
+
+if __name__ == '__main__':
+    d, f = traverse_folder(".", 3)
+    for i in d:
+        print(i)
+    print("-" * 50)
+    for i in f:
+        print(i)
